c4af5091bdf5bb8118b9538675670b5d0712a344
yeasirarafatpp <yeasira79@gmail.com>
2021-08-21 02:03:31 +0600

    Complete All Requirement

--- js/app.js
+++ js/app.js
@@ -1,1 +1,68 @@
-console.log('connected')
+function costForExtra(extra, cost) {
+    const extraPurchase = document.getElementById(extra + '-total');
+    const extraPurchaseText = extraPurchase.innerText;
+    const extraPurchaseUpdate = parseInt(extraPurchaseText) + 1;
+    const newMemoryValue = cost * extraPurchaseUpdate;
+    if (newMemoryValue <= cost) {
+        extraPurchase.innerText = newMemoryValue;
+        const totalPrice = document.getElementById('total-price');
+        const totalPriceText = totalPrice.innerText;
+        const totalPriceRes = parseInt(totalPriceText);
+        totalPrice.innerText = totalPriceRes + newMemoryValue;
+    }
+}
+// Increase Extra Memory Cost
+document.getElementById('extra-memory').addEventListener('click', function () {
+    costForExtra('memory', 180)
+});
+// Increase Extra Storage Cost
+document.getElementById('mid-ssd').addEventListener('click', function () {
+    costForExtra('ssd', 100)
+});
+document.getElementById('tb-ssd').addEventListener('click', function () {
+    costForExtra('ssd', 180);
+});
+// Increase Extra Delivery Cost
+document.getElementById('extra-delivery').addEventListener('click', function () {
+    costForExtra('delivery', 20);
+});
+
+// This Function is for Default Money
+function defaultMoney(product) {
+    const previousAmount = document.getElementById(product + '-total');
+    const previousAmountText = previousAmount.innerText;
+    const previousAmountResult = parseInt(previousAmountText);
+    previousAmount.innerText = previousAmountResult - previousAmountResult;
+    const decreaseTotalPrice = document.getElementById('total-price');
+    const decreaseTotalPriceText = decreaseTotalPrice.innerText;
+    const decreaseTotalPriceRes = parseInt(decreaseTotalPriceText);
+    decreaseTotalPrice.innerText = decreaseTotalPriceRes - previousAmountResult;
+}
+// Default Memory Amount
+document.getElementById('default-memory').addEventListener('click', function () {
+    defaultMoney('memory');
+});
+// Default Storage Amount 
+document.getElementById('default-storage').addEventListener('click', function () {
+    defaultMoney('ssd');
+});
+// Default Delivery Amount 
+document.getElementById('default-delivery').addEventListener('click', function () {
+    defaultMoney('delivery');
+});
+
+// Coupon Code apply
+document.getElementById('coupon-btn').addEventListener('click', function () {
+    const couponCode = document.getElementById('coupon-code').value;
+
+    const couponPrice = document.getElementById('total-price');
+    const couponPriceText = couponPrice.innerText;
+    let couponPriceAmount = parseFloat(couponPriceText);
+    if (couponCode == 'stevekaku') {
+        newCouponPriceAmount = couponPriceAmount / 100 * 20;
+
+        couponPrice.innerText = couponPriceAmount - newCouponPriceAmount;
+    }
+    document.getElementById('coupon-code').value = '';
+
+});
